8625523cf3d4eb8d5d38bb13f0c6c02744620abe
dropling <markus.gertig@web.de>
2020-05-18 16:45:42 +0200

    added file and basic description. No implementation

---
+++ application/scraper.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 18 16:39:08 2020
+
+@author: dropling
+"""
+
+"""
+This file implements the class Scraper which takes as arguments a source
+as well as information that allows the Scraper to navigate the source webpage
+and download the desired information
+"""
+
+class Scraper():
+    pass
