ae528a6e93429655472489d3342ecefb830e792b
dropling <markus.gertig@web.de>
2020-05-18 16:48:36 +0200

    added some description

--- application/entrance.py
+++ application/entrance.py
@@ -5,3 +5,7 @@ Created on Sun May 17 21:20:54 2020
 @author: dropling
 """
 
+"""
+This files implements the application start, involving reading configuration
+files and starting the main thread with the read arguments
+"""
